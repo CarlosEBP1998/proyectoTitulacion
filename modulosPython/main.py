02fb3150c2dd3d56215b7c5d4c82240d1377cb93
Carlos Bautista <caedbape@gmail.com>
2023-06-06 10:59:19 -0600

    MOVEMOS LOS ARCHIVOS DE EduDN a modulosPython

--- modulosPython/main.py
+++ modulosPython/main.py
@@ -4,14 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 # Función para cargar los datos de Excel
-@st.cache
 def load_data(sheet_name):
     data = pd.read_excel('data.xlsx', sheet_name=sheet_name)
     return data
 
 # Función para entrenar el modelo
 def train_model(data):
-    X_train, X_test, y_train, y_test = train_test_split(data.drop('Secuencias Necesarias', axis=1), data['Entrada de Alumnos'], test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(data.drop('target', axis=1), data['target'], test_size=0.2)
     model = RandomForestRegressor()
     model.fit(X_train, y_train)
     return model
@@ -20,10 +19,10 @@ def main():
     st.title('Predicción de Demanda de Secuencias en UPIICSA')
 
     # Carga los datos
-    reprobacion_data = load_data('Índices de Reprobación')
-    bajas_data = load_data('Dictámenes de Baja')
-    ocupabilidad_data = load_data('Ocupabilidad de Materias')
-    entrada_data = load_data('Entrada de Alumnos por Semestre')
+    reprobacion_data = load_data('Índices de Reprobación').copy()
+    bajas_data = load_data('Dictámenes de Baja').copy()
+    ocupabilidad_data = load_data('Ocupabilidad de Materias').copy()
+    entrada_data = load_data('Entrada de Alumnos por Semestre').copy()
 
     # Calcula el número promedio de alumnos por secuencia
     ocupabilidad_data['Alumnos por Secuencia'] = ocupabilidad_data['Total de Alumnos'] / ocupabilidad_data['Secuencia'].nunique()
@@ -33,34 +32,32 @@ def main():
 
     # Crea el DataFrame que usarás para entrenar tu modelo
     data = entrada_data.copy()  # Comienza con los datos de entrada
+    data['target'] = data['Secuencias Necesarias']  # Esto es lo que estás tratando de predecir
 
-    # Verifica que la columna 'Secuencias Necesarias' exista en el DataFrame
-    if 'Secuencias Necesarias' not in data.columns:
-        st.error("Error: La columna 'Secuencias Necesarias' no existe en los datos.")
-        return
+    # Selecciona todas las columnas de tipo 'object' (que suelen ser cadenas de texto en Pandas)
+    categorical_columns = data.select_dtypes(include=['object']).columns
+    # Codifica las columnas categóricas con One-Hot Encoding
+    data_encoded = pd.get_dummies(data, columns=categorical_columns)
 
-    # Selecciona las características necesarias para la predicción
-    features = ['Semestre', 'Entrada de Alumnos']
+    # Verifica si hay columnas faltantes en los datos codificados
+    expected_columns = set(entrada_data.columns)
+    actual_columns = set(data_encoded.columns)
+    missing_columns = expected_columns.difference(actual_columns)
+    if missing_columns:
+        for column in missing_columns:
+            data_encoded[column] = 0
 
-    # Filtra el DataFrame para incluir solo las características necesarias
-    data_filtered = data[features]
-
-    # Verifica que la columna 'Entrada de Alumnos' exista en el DataFrame filtrado
-    if 'Entrada de Alumnos' not in data_filtered.columns:
-        st.error("Error: La columna 'Entrada de Alumnos' no existe en los datos filtrados.")
-        return
-
-    # Entrena el modelo con los datos filtrados
-    model = train_model(data_filtered)
+    # Entrena el modelo con los datos codificados
+    model = train_model(data_encoded)
 
     # Interfaz de usuario para ingresar los valores de entrada y hacer la predicción
     st.subheader('Ingrese los valores de entrada:')
     input_values = {}
-    for column in data_filtered.columns:
+    for column in data.drop(['target'], axis=1).columns:
         input_values[column] = st.number_input(column, value=0.0)
 
     # Genera una lista de valores de entrada en el mismo orden que las columnas del DataFrame
-    input_data = [[input_values[column] for column in data_filtered.columns]]
+    input_data = [[input_values[column] for column in data.drop(['target'], axis=1).columns]]
 
     # Realiza la predicción
     prediction = model.predict(input_data)
@@ -69,5 +66,5 @@ def main():
     st.subheader('Resultado de la predicción:')
     st.write('Número de secuencias necesarias:', prediction[0])
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
