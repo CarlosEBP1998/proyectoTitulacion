db518a4c920e8867cde3caf7ca1b884f40a15c58
Carlos Bautista <caedbape@gmail.com>
2023-06-05 22:21:26 -0600

    Se agrego una pequeña validacion al formulario, para que siempre haya un archivo para enviar

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+
+
+
 app.listen(8080, () => {
     console.log("Servidor en marcha");
 });
--- public/js/cargarDocumento.js
+++ public/js/cargarDocumento.js
@@ -1,11 +1,25 @@
 const uploadForm = document.getElementById('upload-form');
 const fileInput = document.getElementById('file-input');
 const uploadStatus = document.getElementById('upload-status');
+const chargeButton = document.getElementById('chargeButton');
+
+chargeButton.disabled = true;
+
+fileInput.addEventListener('change', function (){
+  console.log(fileInput.files.lenght);
+  if(fileInput.files.length > 0){
+    chargeButton.disabled = false;
+  }
+  else {
+    chargeButton.disabled = true;
+  }
+});
 
 uploadForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const file = fileInput.files[0];
+
     if (file) {
       uploadStatus.textContent = `Cargando: ${file.name}`;
 
