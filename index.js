a6baf99aca0c311f83910f7d3281a13b85da7d3e
Carlos Bautista <caedbape@gmail.com>
2023-06-27 15:17:18 -0600

    Comenzamos a crear el endpoint que permitira el enlace entre las funcionalidade de python y Js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
+app.get('/calculos', (req, res) => {
+    exec('echo "Hola"', (err, stdout, stderr) => {
+        console.log(stdout);
+    });
+});
+
 //VALIDACION DE DOCUMENTOS;
 
 //INVOCAR MODULOS DE PYTHON Y PASAR COMO PARAMETROS LOS DOCUMENTOS;
