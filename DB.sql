ed7a38eb8e324418c1175e9dea3d6279cd25bca3
Carlos Bautista <caedbape@gmail.com>
2023-06-29 06:49:35 -0600

    Definimos un index, insertamos datos en la base de datos

--- DB.sql
+++ DB.sql
@@ -5,10 +5,12 @@ USE users;
 CREATE TABLE IF NOT EXISTS USUARIO (
     nombres varchar(50) NOT NULL,
     apellido_paterno varchar(50) NOT NULL,
-    apellido_paterno varchar(50) NOT NULL,
+    apellido_materno varchar(50) NOT NULL,
     curp varchar(50) NOT NULL,
     nombre_Usuario varchar(50) NOT NULL,
     passw varchar(50) NOT NULL,
     
     PRIMARY KEY (curp)
 );
+
+INSERT INTO USUARIO VALUES ('Victor', 'Perez', 'Dom', 'DPV050568HHGTRR00', 'DacademDPV', '29062023');
